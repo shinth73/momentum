7dfa93197cbc8d3cebd6b6d4e48d97e7e8b5685b
shinth73 <60809725+shinth73@users.noreply.github.com>
2020-05-02 11:11:37 +0900

    Update weather.js

--- src/weather.js
+++ src/weather.js
@@ -1,5 +1,36 @@
 const weather = document.querySelector("js-weather");
+const API_KEY = "d98546f4e44d14f0f49d77370059a45c";
+const COORDS = "coords";
 
-function int() {}
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordObj);
+}
+
+function handleGeoError() {}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+function loadCoords() {
+  const loadedcoord = localStorage.getItem(COOORDS);
+  if (loadCoords === null) {
+    askForCoords();
+  } else {
+  }
+}
+function int() {
+  console.log("sfas");
+  loadCoords();
+}
 
 init();
